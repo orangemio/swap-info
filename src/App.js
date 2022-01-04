ebf919212ef897fe37d5b4a13e4ebf5cb6a9857f
vdovaT <vdova@protonmail.com>
2022-01-04 12:00:18 +0800

    update

--- src/App.js
+++ src/App.js
@@ -115,10 +115,7 @@ function App() {
             </WarningBanner>
           </WarningWrapper>
         )}
-        {globalData &&
-        Object.keys(globalData).length > 0 &&
-        globalChartData &&
-        Object.keys(globalChartData).length > 0 ? (
+        {globalData && globalChartData ? (
           <BrowserRouter>
             <Route component={GoogleAnalyticsReporter} />
             <Switch>
